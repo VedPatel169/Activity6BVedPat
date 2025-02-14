bd321596186188421bfc39f08cedc16bb640eee5
VedPatel169 <ved.patel2ontariotechu.net>
2025-02-13 21:36:48 -0500

    Updated Helloworld.java know prints the message 10 times.

--- Helloworld.java
+++ Helloworld.java
@@ -1,5 +1,7 @@
 public class Helloworld {
     public static void main(String[] args) {
-        System.out.println("Hello World this is activity 6B ved patel");
+        for(int i=1;i<=10;i++) {
+            System.out.println("Hello World this is activity 6B ved patel");
+        }
     }
 }
